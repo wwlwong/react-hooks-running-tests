e67ad68fb1c27326b79fdd6e6a59014d6e8ca10f
William Wong <william.wai.lim.wong@gmail.com>
2023-05-16 11:04:50 -0400

    completed lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -3,9 +3,13 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
